c71eade16183028410e0e94846b412ccc7dd19dc
muaad <muadh24@gmail.com>
2014-06-19 14:28:28 +0300

    added methods for country, country_code, city

--- lib/geoip4rails.rb
+++ lib/geoip4rails.rb
@@ -1,24 +1,27 @@
 require "geoip4rails/version"
+require "httparty"
 
 module Geoip4rails
-  def geolocation ip, format
+  module_function
+  def find_by_ip ip
+  	format = "json"
   	HTTParty.get("http://freegeoip.net/#{format}/#{ip}")
   end
-  
+
   def geolocation
   	ip = request.remote_ip
   	HTTParty.get("http://freegeoip.net/json/#{ip}")
   end
 
-  # def country
-  	
-  # end
-  # def country_code
-  	
-  # end
-  # def city
-  	
-  # end
+  def country ip
+  	find_by_ip(ip)["country_name"]
+  end
+  def country_code ip
+  	find_by_ip(ip)["country_code"]
+  end
+  def city ip
+  	find_by_ip(ip)["city"]
+  end
   # def country
   	
   # end
--- lib/geoip4rails/version.rb
+++ lib/geoip4rails/version.rb
@@ -1,3 +1,3 @@
 module Geoip4rails
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
